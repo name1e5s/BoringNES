efb54049a0985d2f243d6da5e34b3fd6a8faba3d
name1e5s <name1e5s@qq.com>
2019-09-14 23:53:16 +0800

    Load nes file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use nes::utility::read_to_array;
+use std::fs::File;
+use std::path::Path;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut header = [0u8; 16];
+    let path = env::args().nth(1).unwrap_or("sp.nes".to_string());
+    match read_to_array(&mut header, &mut File::open(&Path::new(&path)).unwrap()) {
+        Ok(_x) => {
+            println!("Done")
+        }
+        _ => println!("Fuck")
+    }
+    println!("File path: {:?}", path);
+    println!("Header data: {:?}", header);
 }
